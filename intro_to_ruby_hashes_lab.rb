ebea96e1fb06b7845a443d55d32a3e688e7ae995
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-31 20:52:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,9 +21,9 @@ def monopoly_with_third_tier
      base_hash = {:railroads => 
      {:pieces => 4, 
       :rent_in_dollars => {:one_piece_owned => 25,
-      :two_pieces_owend => 50,
-      :three_level => {},
-      :four_level => {}
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
       },
       :names => {:first_rail => {},
         :second_rail => {},
